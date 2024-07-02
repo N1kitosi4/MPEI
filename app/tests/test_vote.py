69bdfc8c319e97152a6382607d6e2c277fa680f8
N1kitosi4 <129109100+N1kitosi4@users.noreply.github.com>
2024-07-02 20:01:39 +0300

    Update tests

--- app/tests/test_vote.py
+++ app/tests/test_vote.py
@@ -32,3 +32,7 @@ def test_vote_unauthorized_user(client, test_posts):
     res = client.post(
         "/vote", json={"post_id": test_posts[3].id, "dir": 1})
     assert res.status_code == 401
+
+
+def new_branch():
+    ...
